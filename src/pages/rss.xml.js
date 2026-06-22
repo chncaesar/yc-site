e08c5e18c7eb59281661e77a8b47a3ed3c4e6172
Jiachuan Zhu <chncaesar@gmail.com>
2026-06-22 17:09:58 +0800

    fix: update RSS/Header/JSON-LD for new collection URLs

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,19 @@ import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const interviews = await getCollection('interview');
+	const healthPosts = await getCollection('health');
+	const eventPosts = await getCollection('events');
+	const posts = [...interviews, ...healthPosts, ...eventPosts].sort(
+		(a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
+	);
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/blog/${post.id}/`,
+			link: `/${post.collection}/${post.id}/`,
 		})),
 	});
 }
